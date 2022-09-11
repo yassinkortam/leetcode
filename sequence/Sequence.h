9386ebcc72f617063e298e5ce4bb4735ea401a63
Yassin Kortam <yassinkortam@g.ucla.edu>
2022-09-11 00:04:24 -0700

    Added methods and bug fixes

--- sequence/Sequence.h
+++ sequence/Sequence.h
@@ -22,55 +22,69 @@ private:
     Node *_head;
     int _size;
 public:
-    Sequence(); 
     // Create an empty sequence (i.e., one with no items)
-    void print() const; 
+    Sequence(); 
+
+    // Destructor
+    ~Sequence();
+
     //Prints the entire sequence
-    bool empty() const; 
+    void print() const; 
+
     // Return true if the sequence is empty, otherwise false.
-    int size() const;
+    bool empty() const; 
+
      // Return the number of items in the sequence.
+    int size() const;
+
+    // Insert value into the sequence so that it becomes the item at
+    // position pos.  The original item at position pos and those that
+    // follow it end up at positions one higher than they were at before.
+    // Return pos if 0 <= pos < size() and the value could be
+    // inserted.  (It might not be, if the sequence has a fixed capacity,
+    // e.g., because it's implemented using a fixed-size array.)  
+    //Otherwise,
+    // leave the sequence unchanged and return -1.  Notice that
+    // if pos is equal to size(), the value is inserted at the end.
     int insert(int pos, const ItemType& value); 
-        // Insert value into the sequence so that it becomes the item at
-        // position pos.  The original item at position pos and those that
-        // follow it end up at positions one higher than they were at before.
-        // Return pos if 0 <= pos < size() and the value could be
-        // inserted.  (It might not be, if the sequence has a fixed capacity,
-        // e.g., because it's implemented using a fixed-size array.)  
-        //Otherwise,
-        // leave the sequence unchanged and return -1.  Notice that
-        // if pos is equal to size(), the value is inserted at the end.
+
+    // Let p be the smallest integer such that value <= the item at
+    // position p in the sequence; if no such item exists (i.e.,
+    // value > all items in the sequence), let p be size().  Insert
+    // value into the sequence so that it becomes the item at position
+    // p.  The original item at position p and those that follow it end
+    // up at positions one higher than before.  Return p if the value
+    // was actually inserted.  Return -1 if the value was not inserted
+    // (perhaps because the sequence has a fixed capacity and is full).
     int insert(const ItemType& value);
-        // Let p be the smallest integer such that value <= the item at
-        // position p in the sequence; if no such item exists (i.e.,
-        // value > all items in the sequence), let p be size().  Insert
-        // value into the sequence so that it becomes the item at position
-        // p.  The original item at position p and those that follow it end
-        // up at positions one higher than before.  Return p if the value
-        // was actually inserted.  Return -1 if the value was not inserted
-        // (perhaps because the sequence has a fixed capacity and is full).
+
+    // If 0 <= pos < size(), remove the item at position pos from
+    // the sequence (so that all items that followed that item end up at
+    // positions one lower than they were at before), and return true.
+    // Otherwise, leave the sequence unchanged and return false.
     bool erase(int pos);
-        // If 0 <= pos < size(), remove the item at position pos from
-        // the sequence (so that all items that followed that item end up at
-        // positions one lower than they were at before), and return true.
-        // Otherwise, leave the sequence unchanged and return false.
+
+    // Erase all items from the sequence that == value.  Return the
+    // number of items removed (which will be 0 if no item == value).
     int remove(const ItemType& value);
-        // Erase all items from the sequence that == value.  Return the
-        // number of items removed (which will be 0 if no item == value).
+        
+    // If 0 <= pos < size(), copy into value the item at position pos
+    // of the sequence and return true.  Otherwise, leave value unchanged
+    // and return false.
     bool get(int pos, ItemType& value) const;
-        // If 0 <= pos < size(), copy into value the item at position pos
-        // of the sequence and return true.  Otherwise, leave value unchanged
-        // and return false.
+        
+    // If 0 <= pos < size(), replace the item at position pos in the
+    // sequence with value and return true.  Otherwise, leave the sequence
+    // unchanged and return false.
     bool set(int pos, const ItemType& value);
-        // If 0 <= pos < size(), replace the item at position pos in the
-        // sequence with value and return true.  Otherwise, leave the sequence
-        // unchanged and return false.
+        
+    // Let p be the smallest integer such that value == the item at
+    // position p in the sequence; if no such item exists, let p be -1.
+    // Return p.
     int find(const ItemType& value) const;
-        // Let p be the smallest integer such that value == the item at
-        // position p in the sequence; if no such item exists, let p be -1.
-        // Return p.
+       
+    // Exchange the contents of this sequence with the other one.
     void swap(Sequence& other);
-        // Exchange the contents of this sequence with the other one.
 };
 
 Sequence::Sequence(){
@@ -85,13 +99,19 @@ Sequence::Sequence(){
     _size = 0;
 }
 
+Sequence::~Sequence(){
+
+}
+
 void Sequence::print() const{
     Node *temp = new Node;
     temp = _head->next;
-    while (temp != _tail){
-        std::cout << temp->data << "\n";
+    std::cout << "[";
+    while (temp->next != _tail){
+        std::cout << temp->data << ", ";
         temp = temp->next;
     }
+    std::cout << temp->data << "]";
 }
 
 bool Sequence::empty() const{
@@ -101,6 +121,7 @@ bool Sequence::empty() const{
 
 int Sequence::insert(int pos, const ItemType& value){
     if (pos >= _size) return -1;
+
     Node *new_node = new Node; //allocate memory for new node
     new_node->data = value; 
 
@@ -109,8 +130,11 @@ int Sequence::insert(int pos, const ItemType& value){
     for (int i=0; i<pos; i++){
         temp = temp->next;
     }
+
     new_node->next = temp; 
     new_node->prev = temp->prev;
+
+    (temp->prev)->next = new_node;
     temp->prev = new_node; 
 
     _size ++; //increase size by 1
@@ -151,18 +175,104 @@ int Sequence::insert(const ItemType& value){
             return p; 
         }
     }
-    if (temp->data <= value){ //while loop will only break if this is true anyways
-        Node *new_node = new Node;
-        new_node->data = value;
-        new_node->prev = temp->prev;
-        new_node->next = temp;
 
-        (temp->prev)->next = new_node;
-        temp->prev = new_node;
+    Node *new_node = new Node;
+    new_node->data = value;
+    new_node->prev = temp->prev;
+    new_node->next = temp;
 
-        _size++;
-        return p;
-    }
-    return -1; //will never be reached anyways
+    (temp->prev)->next = new_node;
+    temp->prev = new_node;
+
+    _size++;
+    return p;
+    
 }
+
+bool Sequence::erase(int pos){
+    if (pos >= _size) return false;
+
+    Node *temp = new Node;
+    temp = _head->next;
+
+    for (int i=0; i<pos; i++){
+        temp = temp->next;
+    }
+    (temp->next)->prev = temp->prev;
+    (temp->prev)->next = temp->next;
+    free(temp);
+    _size--;
+    return true;
+}
+
+int Sequence::remove(const ItemType& value){
+    if (_size == 0) return 0;
+
+    int pos = 0;
+    Node *temp = new Node;
+    temp = _head;
+
+    while (temp != _tail){
+        if (temp->data == value){
+            (temp->next)->prev = temp->prev;
+            (temp->prev)->next = temp->next;
+            free(temp);
+            return pos;
+        }
+        temp = temp->next;
+        pos++;
+    }
+    return 0;
+}
+
+bool Sequence::get(int pos, ItemType& value) const{
+    if (pos >= _size) return false;
+
+    Node *temp = new Node;
+    temp = _head->next;
+
+    for (int i=0; i<pos; i++){
+        temp = temp->next;
+    }
+
+    value = temp->data;
+    return true;
+}
+
+bool Sequence::set(int pos, const ItemType& value){
+    if (pos >= _size) return false;
+
+    Node *temp = new Node;
+    temp = _head->next;
+
+    for (int i=0; i<pos; i++){
+        temp = temp->next;
+    }
+
+    temp->data = value;
+    return true;
+}
+
+int Sequence::find(const ItemType& value) const{
+    if (_size == 0) return -1;
+
+    int p = 0;
+    Node *temp = new Node;
+    temp = _head->next;
+
+    while (temp != _tail){
+        if (temp->data == value){
+            return p;
+        }
+        temp = temp->next;
+        p++;
+    }
+    return -1;
+}
+
+void Sequence::swap(Sequence& other){
+    
+
+}
+
 
--- sequence/main.cpp
+++ sequence/main.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
+#include <string>
 #include "Sequence.h"
 
 int main(){
     Sequence sequence = Sequence();
-    sequence.insert("First");
-    sequence.insert("Hello World!");
+
+    //Alphabetical insertion
+    sequence.insert("A");
+    sequence.insert("B");
+    sequence.insert("C");
+    sequence.insert("D");
+    sequence.insert("E");
+    sequence.insert("F");
+
+    //Insertion at given index
+    sequence.insert(0, "G");
+
+    std::cout << "GFEDCBA: ";
     sequence.print();
+    std::cout << "\n";
+
+    //Erasure at given index
+    sequence.erase(0);
+
+    std::cout << "FEDCBA: ";
+    sequence.print();
+    std::cout << "\n";
+
+    //Removal of a given value
+    sequence.remove("F");
+
+    std::cout << "EDCBA: ";
+    sequence.print();
+    std::cout << "\n";
+
+    //Get value at given position
+    std::string value = "";
+    sequence.get(4, value);
+
+    std::cout << "A: " << value << "\n";
+
+    //Set data at given position
+    sequence.set(0, value);
+
+    std::cout << "ADCBA: ";
+    sequence.print();
+    std::cout << "\n";
+
+    //Find given data 
+    std::cout << "First instance of A is at: " << sequence.find("A") <<"\n";
+
+
+
+
+
+
 }
